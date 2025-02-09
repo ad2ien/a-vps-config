058a9a36e4637969febcb7034e279d5ccd3f29cc
Adrien Touzouli <contact@ad2ien.dev>
2025-02-09 17:33:54 +0100

    ♻️ move Registry to Ansible

--- playbooks/roles/08_registry/files/setup.sh
+++ playbooks/roles/08_registry/files/setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 set -e
 
 mkdir -p auth
 source .env
 
-docker run \
+docker run --rm \
   --entrypoint htpasswd \
   httpd:2 -Bbn $REGISTRY_AUTH_USER $REGISTRY_AUTH_PW > auth/htpasswd
